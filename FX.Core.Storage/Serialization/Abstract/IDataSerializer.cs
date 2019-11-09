a6560ef2ea3f542f4cb7948f4b4f83c0d664471c
Marian Focsa <marian.focsa@outlook.com>
2019-11-09 23:30:25 +0200

    Add 'Storage' project with basic interface and memory serializer

---
+++ FX.Core.Storage/Serialization/Abstract/IDataSerializer.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+
+namespace FX.Core.Storage.Serialization.Abstract
+{
+    public interface IDataSerializer
+    {
+
+        /// <summary>
+        /// Specifies if a data (represented by an ID) exista as Saved
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <returns></returns>
+        bool DataExists(string guid);
+
+        /// <summary>
+        /// Returns data (of type T) from storage, if it exists
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="guid"></param>
+        /// <returns></returns>
+        Task<T> GetData<T>(string guid) where T : class;
+
+        /// <summary>
+        /// Saves data (identified by 'identifier') using the specific implementation
+        /// </summary>
+        /// <param name="response"></param>
+        /// <param name="identifier"></param>
+        /// <returns></returns>
+        Task SaveDataAsync(object response, string identifier);
+
+        /// <summary>
+        /// Deletes data that is saved, based on ID
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <returns></returns>
+        Task DeleteDataAsync(string guid);
+    }
+}
---
+++ FX.Core.Storage/Serialization/Implementations/MemoryDataSerializer.cs
@@ -0,0 +1,106 @@
+﻿using FX.Core.Storage.Serialization.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FX.Core.Storage.Serialization.Implementations
+{
+    public class MemoryDataSerializer: IDataSerializer, IDisposable
+    {
+        private ReaderWriterLockSlim _activeDataLock;
+        private Dictionary<string, object> _temporaryData;
+
+        public MemoryDataSerializer()
+        {
+            _activeDataLock = new ReaderWriterLockSlim();
+            _temporaryData = new Dictionary<string, object>();
+        }
+
+        public bool DataExists(string guid)
+        {
+            _activeDataLock.EnterReadLock();
+            try
+            {
+                return !(_temporaryData == null || !_temporaryData.ContainsKey(guid));
+            }
+            finally
+            {
+                _activeDataLock.ExitReadLock();
+            }
+        }
+
+        public Task<T> GetData<T>(string guid) where T : class
+        {
+            if (!DataExists(guid))
+                return Task.FromResult<T>(null);
+
+            _activeDataLock.EnterReadLock();
+            try
+            {
+                return Task.FromResult(_temporaryData[guid] as T);
+            }
+            finally
+            {
+                _activeDataLock.ExitReadLock();
+            }
+        }
+
+        public Task SaveDataAsync(object response, string identifier)
+        {
+            _activeDataLock.EnterWriteLock();
+            try
+            {
+                if (_temporaryData == null)
+                    _temporaryData = new Dictionary<string, object>();
+
+                _temporaryData.Add(identifier, response);
+            }
+            finally
+            {
+                _activeDataLock.ExitWriteLock();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteDataAsync(string guid)
+        {
+            _activeDataLock.EnterWriteLock();
+            try
+            {
+                var dict = _temporaryData;
+                if (dict != null && dict.ContainsKey(guid))
+                {
+                    dict[guid] = null;
+                    dict.Remove(guid);
+                }
+            }
+            finally
+            {
+                _activeDataLock.ExitWriteLock();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _activeDataLock.EnterWriteLock();
+            try
+            {
+                if (_temporaryData != null)
+                    _temporaryData.Clear();
+
+                _temporaryData = null;
+            }
+            finally
+            {
+                _activeDataLock.ExitWriteLock();
+            }
+
+            _activeDataLock.Dispose();
+            _activeDataLock = null;
+        }
+    }
+}
