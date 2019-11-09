1c74bdb8b8daa8ada082badedb60340e70f89f26
Marian Focsa <marian.focsa@outlook.com>
2019-11-09 21:44:32 +0200

    Add Logging Project, with NLog Console Wrapper

---
+++ FX.Core.Logging/NLog/Implementations/LoggerFactory.cs
@@ -0,0 +1,48 @@
+﻿using NLog;
+using NLog.Config;
+using System.Linq;
+using NL = NLog;
+
+namespace FX.Core.Logging.NLog.Implementations
+{
+    public class LoggerFactory
+    {
+        /// <summary>
+        /// Reads the config file for NLog from an XML file (use if you don't want the default config load).
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public static LoggingConfiguration LoadConfigFromFile(string filePath)
+        {
+            return new XmlLoggingConfiguration(filePath);
+        }
+
+        /// <summary>
+        /// Returns a console logger. Must be initialized with the configuration.
+        /// The configuration can be read:
+        /// - manually from the config file (with LoadConfigFromFile)
+        /// - initialized programatically 
+        /// - provided from LogManager.Configuration if in the application directory there's a nlog.config file
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static ILogger CreateConsoleLogger(LoggingConfiguration config)
+        {
+            if (config != null)
+                LogManager.Configuration = config;
+
+            if (LogManager.Configuration == null)
+                throw new System.Exception("NLog initialized with invalid config");
+
+            var target = LogManager.Configuration.AllTargets.FirstOrDefault(x => x.Name == "logConsole");
+
+            if (target == null) // add console target if not configured
+            {
+                var logConsole = new NL.Targets.ConsoleTarget("logConsole");
+                LogManager.Configuration.AddRule(LogLevel.Trace, LogLevel.Fatal, logConsole);
+            }
+
+            return LogManager.GetCurrentClassLogger();
+        } 
+    }
+}
