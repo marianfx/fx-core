d19db00eef4fab78f7b675e2d576414d73ccdea3
Marian Focsa <marian.focsa@outlook.com>
2019-11-10 16:17:43 +0200

    Add IBasicSettings handler (load + save settings interface)

---
+++ FX.Core.Common/Settings/Abstract/IBasicSettings.cs
@@ -0,0 +1,23 @@
+﻿using FX.Core.Common.Settings.Models;
+
+namespace FX.Core.Common.Settings.Abstract
+{
+    public interface IBasicSettings<TSettings>
+        where TSettings: ISettingsObject
+    {
+        /// <summary>
+        /// Represents the current settings
+        /// </summary>
+        TSettings Settings { get; set; }
+
+        /// <summary>
+        /// Loads the settings, using the inner loading mechanism
+        /// </summary>
+        void LoadSettings();
+
+        /// <summary>
+        /// Save the settings (if they've been edited) using the inner saving mechanisms
+        /// </summary>
+        void SaveSettings();
+    }
+}
---
+++ FX.Core.Common/Settings/Models/ISettingsObject.cs
@@ -0,0 +1,11 @@
+﻿namespace FX.Core.Common.Settings.Models
+{
+    public interface ISettingsObject
+    {
+        /// <summary>
+        /// Validate the current settings object. Returns true if there are errors.
+        /// </summary>
+        /// <returns></returns>
+        bool Validate();
+    }
+}
