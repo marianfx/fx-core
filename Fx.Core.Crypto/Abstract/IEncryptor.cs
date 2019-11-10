e5386bda7959d7f0d0efaf76b5d254437f45ac80
Marian Focsa <marian.focsa@outlook.com>
2019-11-10 16:19:33 +0200

    Add IEncryptor (encrypt + decrypt based on pwd) with AES HMAC implementation
    -- in a  new crypto project

---
+++ Fx.Core.Crypto/Abstract/IEncryptor.cs
@@ -0,0 +1,8 @@
+﻿namespace Fx.Core.Crypto.Abstract
+{
+    public interface IEncryptor
+    {
+        string Encrypt(string value, string key);
+        string Decrypt(string value, string key);
+    }
+}
---
+++ Fx.Core.Crypto/Implementations/AesHmacEncryptor.cs
@@ -0,0 +1,20 @@
+﻿using Fx.Core.Crypto.Abstract;
+using Fx.Core.Crypto.Algo;
+
+namespace Fx.Core.Crypto.Implementations
+{
+    public class AesHmacEncryptor : IEncryptor
+    {
+        public string Decrypt(string value, string key)
+        {
+            var keyToUse = AesHmacEncryption.MapTo12Characters(key);
+            return AesHmacEncryption.SimpleDecryptWithPassword(value, keyToUse);
+        }
+
+        public string Encrypt(string value, string key)
+        {
+            var keyToUse = AesHmacEncryption.MapTo12Characters(key);
+            return AesHmacEncryption.SimpleEncryptWithPassword(value, keyToUse);
+        }
+    }
+}
