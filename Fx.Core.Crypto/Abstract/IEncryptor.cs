3b376046dfe8494d03ced80132effe71dd9c88c0
Marian Focsa <marian.focsa@kireygroup.com>
2020-01-16 18:38:00 +0200

    Add description for IEncryptor interface

--- Fx.Core.Crypto/Abstract/IEncryptor.cs
+++ Fx.Core.Crypto/Abstract/IEncryptor.cs
@@ -2,7 +2,20 @@
 {
     public interface IEncryptor
     {
+        /// <summary>
+        /// Encrypts the given string value, with the given key (a key-based encryptor)
+        /// </summary>
+        /// <param name="value">The text to encrypt</param>
+        /// <param name="key">The key to use for encryption</param>
+        /// <returns></returns>
         string Encrypt(string value, string key);
+
+        /// <summary>
+        /// Decrypts the given string value, with the given key (a key-based decryptor)
+        /// </summary>
+        /// <param name="value">The text to decrypt</param>
+        /// <param name="key">The key to use for decryption</param>
+        /// <returns></returns>
         string Decrypt(string value, string key);
     }
 }
